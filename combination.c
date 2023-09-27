bc1829b07030a54a17bd8d47ca0104ea2b34d4bc
dada <dada_math@outlook.com>
2023-09-27 17:47:46 +0900

    modified:   combination.c

--- combination.c
+++ combination.c
@@ -1,23 +1,23 @@
-/* 
- * Combination (n, r): get r combinations among n elements 
- * 
+/*
+ * Combination (n, r): get r combinations among n elements
+ *
  */
 
 #include <stdio.h>
 
-int main(void){
-    int n = 6;
-    int r = 3;
-    //int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+int main(void) {
+  int n = 6;
+  int r = 3;
+  // int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-    for(int i=1;i<=n-2;i++){
-        for(int j=i+1;j<=n-1;j++){
-            for(int k=j+1;k<=n;k++){
-                //printf("%d %d %d\n", a[i], a[j], a[k]);
-                printf("%d %d %d\n", i, j, k);
-            }
-        }
+  for (int i = 1; i <= n - 2; i++) {
+    for (int j = i + 1; j <= n - 1; j++) {
+      for (int k = j + 1; k <= n; k++) {
+        // printf("%d %d %d\n", a[i], a[j], a[k]);
+        printf("%d %d %d\n", i, j, k);
+      }
     }
+  }
 
-    return 0;
+  return 0;
 }
