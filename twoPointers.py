4f6138ebe985666a9ff3e7ddd609617ec0a158e7
dada <dada_math@outlook.com>
2023-11-03 12:54:32 +0900

    new file:   AG0x. Two Pointers.md 	new file:   twoPointers.py

---
+++ twoPointers.py
@@ -0,0 +1,16 @@
+a = [1, 2, 3, 4, 2, 5, 3, 1, 1, 2]
+M = 5
+
+lenA = len(a)
+
+start, end = 0, 0
+while start < lenA:
+  # print(f"[{start}:{end}] ", end='')
+  sumP = sum(a[start:end])
+  if sumP < M and end < lenA:
+    end += 1
+  elif sumP > M or end == lenA:
+    start += 1
+  elif sumP == M:
+    print("found =", a[start:end])
+    start += 1
