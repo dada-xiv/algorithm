38cfc66e9c9abec0ef24872e74d3a41125156454
dada <dada_math@outlook.com>
2023-12-15 15:46:43 +0900

    modified:   euclidean.py

--- euclidean.py
+++ euclidean.py
@@ -1,8 +1,14 @@
 def getGCD(m, n):
-	while n!=0:
-		r = m%n
-		m = n
-		n = r
-	return m
+  while n != 0:
+    r = m % n
+    m = n
+    n = r
+  return m
+
+def gcd(m, n):
+  while n:
+    m, n = n, m % n
+  return m
 
 print(getGCD(40, 232))
+print(gcd(40, 232))
